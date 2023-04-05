381087c8627d9d77d1f74819f91fb36ae2102773
Thanh-cong VO <tvo@e3r1p3.clusters.42paris.fr>
2023-04-05 20:40:16 +0200

    update fizzbuzz

--- lv0/fizzbuzz/fizzbuzz.c
+++ lv0/fizzbuzz/fizzbuzz.c
@@ -6,42 +6,37 @@
 /*   By: tvo <tvo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 20:44:38 by tvo               #+#    #+#             */
-/*   Updated: 2023/04/05 20:27:17 by tvo              ###   ########.fr       */
+/*   Updated: 2023/04/05 20:37:40 by tvo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
-void	write_number(int	nb)
+void	write_word(int nb)
 {
 	char	str[] = "0123456789";
 
 	if (nb > 9)
-	{
-		write_number(nb / 10);
-	}
+		write_word(nb / 10);
 	write(1, &str[nb % 10], 1);
 }
 
-
-
-int	main()
+int main()
 {
-	int	nb = 1;
+	int	nb;
 
+	nb = 1;
 	while (nb <= 100)
 	{
-		if ((nb % 3) == 0)
+		if (((nb % 3) == 0) && ((nb % 5) == 0))
+			write(1, "fizzbuzz", 8);
+		else if ((nb % 3) == 0)
 			write(1, "fizz", 4);
 		else if ((nb % 5) == 0)
-			write(1, "fuzz", 4);
-		else if ((nb % 3) == 0 && (nb % 5) == 0)
-			write(1, "fizzbuzz", 8);
+			write(1, "buzz", 4);
 		else
-			write_number(nb);
+			write_word(nb);
 		write(1, "\n", 1);
 		nb++;
 	}
-	return (0);
 }
