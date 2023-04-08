5f81962bbf1c0851a8a55bee84575b4ad40953f9
Thanh-cong VO <tvo@e1r1p13.clusters.42paris.fr>
2023-04-08 18:50:18 +0200

    rotone update

--- lv0/rotone/rotone.c
+++ lv0/rotone/rotone.c
@@ -6,40 +6,34 @@
 /*   By: tvo <tvo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 17:33:20 by tvo               #+#    #+#             */
-/*   Updated: 2023/04/08 17:48:47 by tvo              ###   ########.fr       */
+/*   Updated: 2023/04/08 18:49:45 by tvo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	rotone(char ch)
+void	ft_putchar(char ch)
 {
-	if ((ch >= 'a' && ch < 'z') || (ch >= 'A' && ch < 'Z'))
+	write(1, &ch, 1);
+}
+
+void	rotone(char *av)
+{
+	while (*av)
 	{
-		ch++;
-		write(1, &ch, 1);
+		if ((*av >= 'a' && *av < 'z') || (*av >= 'A' && *av < 'Z'))
+			ft_putchar(*av + 1);
+		else if (*av == 'z' || *av == 'Z')
+			ft_putchar(*av - 25);
+		else
+			ft_putchar(*av);
+		++av;
 	}
-	else if (ch == 'z' || ch == 'Z')
-	{
-		ch -= 25;
-		write(1, &ch, 1);
-	}
-	else
-		write(1, &ch, 1);
 }
 
 int main(int ac, char **av)
 {
 	if (ac == 2)
-	{
-		int	i;
-
-		i = 0;
-		while (av[1][i])
-		{
-			rotone(av[1][i]);
-			i++;
-		}
-	}
+		rotone(av[1]);
 	write(1, "\n", 1);
 }
